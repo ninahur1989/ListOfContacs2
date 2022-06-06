14dc263a5b75d70348bdd93212922603df8cf607
stas <79771234+ninahur1989@users.noreply.github.com>
2022-06-07 00:04:01 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListOfContacs2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
